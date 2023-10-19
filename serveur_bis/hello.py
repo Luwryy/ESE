e048b8ae9f22acadc201e8bc1f8020dabc6420ea
pizero <louislienart@hotmail.fr>
2023-10-19 09:52:18 +0100

    working server tp3

---
+++ serveur_bis/hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import jsonify
+import json
+
+app = Flask(__name__)
+
+welcome = 'welcome to ESE API'
+
+@app.route('/api/welcome/')
+def api_welcome() :
+    return welcome
+
+@app.route('/api/welcome<int:index>')
+def api_welcome_index(index) :
+    return json.dumps({"index": index,"val": welcome[index]}), {"Content-Type": "application/json"}
+
+@app.errorhandler(404)
+def page_not_found(error) :
+    return render_templates('page_not_found.html'), 404
