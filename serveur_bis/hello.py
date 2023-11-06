9c2f8ea801ab0bcc92f21086a365e54bc4f5b0bc
pizero <louislienart@hotmail.fr>
2023-11-06 10:30:09 +0000

    server pi

--- serveur_bis/hello.py
+++ serveur_bis/hello.py
@@ -12,7 +12,7 @@ welcome = 'welcome to ESE API'
 def api_welcome() :
     return welcome
 
-@app.route('/api/welcome<int:index>')
+@app.route('/api/welcome<int:index>', methods=['GET','POST'])
 def api_welcome_index(index) :
     if (len(welcome) <= (index)) :
         return abort(404)
@@ -22,3 +22,31 @@ def api_welcome_index(index) :
 @app.errorhandler(404)
 def page_not_found(error) :
     return templating.render_template('page_not_found.html'), 404
+
+#, 'PUT', 'PATCH', 'DELETE'
+
+@app.route('/api/request/', methods=['GET', 'POST'])
+@app.route('/api/request/<path>', methods=['GET','POST'])
+def api_request(path=None):
+    resp = {
+            "method":   request.method,
+            "url" :  request.url,
+            "path" : path,
+            "args": request.args,
+            "headers": dict(request.headers),
+    }
+    if request.method == 'POST':
+        resp["POST"] = {
+                "data1" : 5,
+                "data" : request.get_json(),
+                }
+    if request.method == 'GET':
+        if path == 'welcome/' :
+            resp["GET"] = {
+                "data" : request.get_json(),
+                }
+        else :
+            resp["GET"] = {
+                "data" : request.get_json(),
+                }
+    return jsonify(resp)
